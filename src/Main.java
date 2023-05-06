edc5a887cff93bed8850fbfd443a128e9d25e60a
Christopher Pam <chrystopam@gmail.com>
2023-05-06 13:34:13 +0100

    [feat]: added validation

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,36 @@ public class Main {
         byte MONTHS_IN_YEAR = 12;
         byte PERCENT = 100;
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal;
+        while(true) {
+            System.out.print("Principal (1k - 1M): ");
+            principal = scanner.nextInt();
+            if (!(1000 <= principal) || !(principal <= 1_000_000))
+                System.out.println("Enter a number between 1000 and 1000000");
+            else
+                break;
+        }
 
-        System.out.print("Annual Interest Rate(%): ");
-        float interestRate = scanner.nextFloat();
+        float interestRate;
+        while(true) {
+            System.out.print("Annual Interest Rate(%): ");
+            interestRate = scanner.nextFloat();
+            if (!(0 <= interestRate) || !(interestRate <= 30))
+                System.out.println("Enter a number between 0 and 30");
+            else
+                break;
+        }
         float monthlyInterestRate = (interestRate / PERCENT) / MONTHS_IN_YEAR;
 
-        System.out.print("Period(Years): ");
-        byte period = scanner.nextByte();
+        byte period;
+        while(true) {
+            System.out.print("Period(Years): ");
+            period = scanner.nextByte();
+            if (!(0 < period) || !(period <= 30))
+                System.out.println("Enter a value between 1 and 30");
+            else
+                break;
+        }
         int numberOfPayments = period * MONTHS_IN_YEAR;
 
         double mortgage = principal
